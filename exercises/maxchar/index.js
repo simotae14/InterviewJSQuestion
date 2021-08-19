2c86ef048d6ab0473bc152729afc680f3fe4fbec
Simone <startae14@gmail.com>
2021-08-19 23:04:35 +0200

    09: maxChar exercise, my solution using forEach to create the map object with every character and its occurencies and a reduce to retrieve the maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const mapStringCharObject = {};
+  [...str].forEach(c => mapStringCharObject[c] = mapStringCharObject[c] + 1 || 1);
+  let maxChar = str[0];
+  Object.keys(mapStringCharObject).reduce((maxValue, currentValue) => {
+    const max = maxValue > mapStringCharObject[currentValue] ? maxValue : mapStringCharObject[currentValue];
+    if (maxValue < mapStringCharObject[currentValue]) {
+      maxChar = currentValue;
+    }
+    return max;
+  }, 0);
+  return maxChar;
+}
 
 module.exports = maxChar;
