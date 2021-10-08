60988ae0beb1f972423dd27a211875d8606db0c0
Simone Taeggi <startae14@gmail.com>
2021-10-08 12:20:33 +0200

    16: Anagrams: Stephen first solution using regexp, a create Character map funtion helper and for in to loop the Character map

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,10 +8,11 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-/* 1: My solution using Regexp and replace to clean strings
+/* 1: My solution using Regexp and replace it to clean strings
       counting the number of keys in the First Character Map and in the second
       and comparing the 2 Character maps
 */
+/*
 function generateCharacterMap(stringVal) {
   let charMap = {};
   [...stringVal].forEach((c) => {
@@ -37,6 +38,33 @@ function anagrams(stringA, stringB) {
     return true;
   }
   return false;
+}
+*/
+
+/* 2: Stephen's solution using an helper method to create the Characters Map, check if the strings
+  has the same amount of keys with Object.keys(obj).length
+*/
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+  // compare the two char maps
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+// helper function Characters Map
+function buildCharMap(str) {
+  const charMap = {};
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
 }
 
 module.exports = anagrams;
