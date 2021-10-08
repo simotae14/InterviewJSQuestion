9770bb9de4e641a6e32e7da6399684a9509fb0fb
Simone Taeggi <startae14@gmail.com>
2021-10-08 16:03:58 +0200

    18: Sentence Capitalization: my solution using split, map, toUpperCase and slice methods

--- exercises/capitalize/starter.js
+++ exercises/capitalize/starter.js
@@ -6,12 +6,7 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
-/* 
-My solution: using the splice method to divede the sentence in words and
-*/
-function capitalize(str) {
-  const splittedString = str.split(" ").map(word => word[0].toUpperCase() + word.slice(1));
-  return splittedString.join(" ");
-}
+
+function capitalize(str) {}
 
 module.exports = capitalize;
