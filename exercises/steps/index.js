40828a299535b5bc332814b0cbb2b4e927a660bf
Simone Taeggi <startae14@gmail.com>
2021-10-11 17:17:09 +0200

    22: Steps: Stephen's first solution, the iterative solution using two for loops for all the rows and columns

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,9 +17,30 @@
 //       '### '
 //       '####'
 
+/*
+My solution: using a simple for loop and a console log
+*/
+/*
 function steps(n) {
   for (let i = 1; i <= n; i++) {
     console.log("#".repeat(i) + " ".repeat(n - i));
+  }
+}
+*/
+/*
+Stephen's first solution: iterative and easier solution
+*/
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let col = 0; col < n; col++) {
+      if (col <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
   }
 }
 
